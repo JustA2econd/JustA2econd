cf2d77eae4dab15cbadfad2d3fb8021b85f23d91
E Miranda <155202247+JustA2econd@users.noreply.github.com>
2024-12-02 15:37:43 -0800

    Adjusted player physics
    Player now loses speed when hitting a wall, and can no longer jump mid-air (but they can wall jump)

--- player.lua
+++ player.lua
@@ -10,27 +10,34 @@ function Player:new(x, y)
     -- Speed
     self.speed_x = 0
     self.speed_y = 0
-    -- Touching level?
-    self.ground = false
-    self.wall = false
+    -- Other stuff
+    self.cols = {}
+    self.cols_len = 0
+    self.normal_x = 0
+    self.normal_y = 0
 end
 
 function Player:update(dt)
     -- Accelerate right
     if love.keyboard.isDown("right") and not love.keyboard.isDown("left") then
-        self.speed_x = self.speed_x + 1000 * dt
+        self.speed_x = self.speed_x + 1500 * dt
     -- Accelerate left
     elseif love.keyboard.isDown("left") and not love.keyboard.isDown("right") then
-        self.speed_x = self.speed_x - 1000 * dt
+        self.speed_x = self.speed_x - 1500 * dt
     -- If not touching one key, slow down
     else
         if self.speed_x >= 10 then
-            self.speed_x = self.speed_x - 1000 * dt
+            self.speed_x = self.speed_x - 1500 * dt
         elseif self.speed_x <= -10 then
-            self.speed_x = self.speed_x + 1000 * dt
+            self.speed_x = self.speed_x + 1500 * dt
         else
             self.speed_x = 0
         end
+    end
+
+    -- Jump
+    if love.keyboard.isDown("up") and self.normal_y == -1 then
+        self.speed_y = -400
     end
 
     -- Terminal velocity (x)
@@ -44,12 +51,26 @@ function Player:update(dt)
     self.x = self.x + self.speed_x * dt
     self.y = self.y + self.speed_y * dt
 
-    -- Adjust for collision
-    self.x, self.y = world:move(player, self.x, self.y)
+    -- Check for collision and adjust
+    self.normal_x = 0
+    self.normal_y = 0
+    self.x, self.y, self.cols, self.cols_len = world:move(player, self.x, self.y)
+    if self.cols[1] then
+        for i, collision in ipairs(self.cols) do
+            if collision.normal.x == 1 or collision.normal.x == -1 then
+                self.speed_x = 0
+                self.normal_x = collision.normal.x
+            end
+            if collision.normal.y == 1 or collision.normal.y == -1 then
+                self.speed_y = 0
+                self.normal_y = collision.normal.y
+            end
+        end
+    end
 
-    -- Change gravity
-    if not self.ground then
-        self.speed_y = self.speed_y + 500 * dt
+    -- Apply gravity
+    if self.normal_y == 0 then
+        self.speed_y = self.speed_y + 700 * dt
     else
         self.speed_y = 0
     end
@@ -63,6 +84,15 @@ end
 
 function Player:keypressed(key)
     if key == "up" then
-        self.speed_y = -400
+        -- Handle walljumping
+        if self.normal_y == -1 then
+            -- Do nothing (don't wall jump if on the floor)
+        elseif self.normal_x == 1 then
+            self.speed_y = -250
+            self.speed_x = 400
+        elseif self.normal_x == -1 then
+            self.speed_y = -250
+            self.speed_x = -400
+        end
     end
 end
